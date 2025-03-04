acf9ccbef830518e51ac0e9cc33b0186e2d17a11
Endaah <marcosgg2002@gmail.com>
2025-03-04 21:55:41 +0100

    Añadido constructor a Question

--- gameservice/questions/Question.ts
+++ gameservice/questions/Question.ts
@@ -1,6 +1,13 @@
 
 
 export class Question {
+
+    constructor(imageUrl: string, options: string[], correctAnswer: string) {
+        this.imageUrl = imageUrl;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+
     imageUrl: string;
     options: string[];
     correctAnswer: string;
