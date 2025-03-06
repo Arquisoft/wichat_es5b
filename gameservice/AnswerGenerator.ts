bf9f7319b05ae6106bd6e9ade17d9d9546ac81c9
Endaah <UO282587@uniovi.es>
2025-03-06 10:23:03 +0100

    Quitada pregunta hardcodeada

--- gameservice/AnswerGenerator.ts
+++ gameservice/AnswerGenerator.ts
@@ -4,7 +4,7 @@ export class AnswerGenerator {
 
     generateAnswers(correctAnswer: string): string[] {
         //TODO: Hacer que las respuestas sean aleatorias
-        const answers = [correctAnswer, "ULTRAKILL", "Elden Ring", "Hollow Knight"]
+        const answers = [correctAnswer, "Respuesta 1", "Respuesta 2", "Respuesta 3"]
         return answers.sort(() => Math.random() - 0.5);
     }
 
--- gameservice/QuestionGenerator.ts
+++ gameservice/QuestionGenerator.ts
@@ -5,7 +5,7 @@ export class QuestionGenerator {
     generateQuestion(): Question {
         // Implement the logic to generate a question here
         // Return the generated question
-        return new Question("./images/votd_skybox.jpg", [], "Destiny 2");
+        return new Question("", [], "Respuesta correcta");
       }
 
 }
