4fea5913476c60191c21d127270440769f49869b
UO222898 <UO222898@uniovi.es>
2025-03-11 21:21:56 +0100

    Arreglados los conflictos con la rama "implementacion-wikidata-logica-juego"

--- gameservice/src/GameController.ts
+++ gameservice/src/GameController.ts
@@ -1,3 +1,6 @@
+import express from "express";
+import cors from "cors";
+
 import { QuestionManager } from "./QuestionManager";
 import { AnswerVerifier } from "./AnswerVerifier";
 import { Question } from "./questions/Question";
@@ -72,15 +75,19 @@ export class GameController {
         selectedAnswer,
         this.currentQuestion.getCorrectAnswer()
       );
-  
-      if (isCorrect) {
-        this.score++;
-        console.log("¡Respuesta correcta! Puntuación:", this.score);
-        this.nextQuestion();
-        console.log(this.hasGameEnded);
-      } else {
+      
+      if (!isCorrect) {
         console.log("Respuesta incorrecta.");
         this.endGame();
+      } else {
+        this.score++;
+        console.log("¡Respuesta correcta! Puntuación:", this.score);
+        if(!this.questionManager.areThereQuestionsLeft()){
+          this.endGame();
+        } else {
+          this.nextQuestion();
+          console.log(this.hasGameEnded);
+        }
       }
     }
   
@@ -96,3 +103,40 @@ export class GameController {
       return this.currentQuestion;
     }
   }
+
+const questionGen = new QuestionManager();
+const answerVer = new AnswerVerifier();
+const gameController = new GameController(questionGen, answerVer);
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Petición para iniciar el juego
+app.post("/start", (req: any, res: any) => {
+  gameController.startGame();
+  res.sendStatus(200);
+});
+
+// Petición para terminar el juego
+app.post("/end", (req: any, res: any) => {
+  gameController.endGame();
+  res.sendStatus(200);
+});
+
+// Petición para obtener la pregunta actual
+app.get("/question", (req: any, res: any) => {
+  const question = gameController.getCurrentQuestion();
+  res.json(question);
+});
+
+// Petición para obtener respuesta
+app.post("/answer", (req: any, res: any) => {
+  const selectedAnswer = req.body.answer;
+  gameController.submitAnswer(selectedAnswer);
+  res.sendStatus(200);
+});
+
+app.listen(8005, () => {
+  console.log("Servidor iniciado en el puerto 8005");
+});
