bd69c14887a64876a62a61bec9a13b6a5b0c063a
jmonteguillem <jmonteguillem@gmail.com>
2025-03-18 19:33:42 +0100

    Arrglos para que se vea la primera pregunta

--- webapp/src/components/game/GameQuestion.jsx
+++ webapp/src/components/game/GameQuestion.jsx
@@ -7,9 +7,9 @@ import axios from 'axios';
 const gameUrl = process.env.GAMECONTROLLER_URL || 'http://localhost:8005';
 
 export default function MovieQuiz() {
-  const [currentQuestion, setCurrentQuestion] = useState("");
+  const [currentQuestion, setCurrentQuestion] = useState(null);
   const [selectedOption, setSelectedOption] = useState(null);
-  const [timeLeft, setTimeLeft] = useState(30);
+  const [timeLeft, setTimeLeft] = useState(null);
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [wrongAnswers, setWrongAnswers] = useState(0);
   const [gameFinished, setGameFinished] = useState(false);
@@ -17,6 +17,10 @@ export default function MovieQuiz() {
   const [loading, setLoading] = useState(true);
   const PREGUNTASNUM = 6;
 
+  useEffect(() => {
+    console.log("Componente montado, llamando a nextQuestion");
+    nextQuestion();
+  }, []);
 
   const nextQuestion = async () => {
     
@@ -32,12 +36,12 @@ export default function MovieQuiz() {
 
     setCurrentQuestion(question);
     setSelectedOption(null);
-    setTimeLeft(10);
     setLoading(false);
-    
+    setTimeLeft(30);
   };
 
   useEffect(() => {
+    if (timeLeft === null || loading) return;
     
     if (timeLeft === 0) {
       // setWrongAnswers((prev) => prev + 1);
@@ -60,7 +64,7 @@ export default function MovieQuiz() {
     }, 1000);
   
     return () => clearInterval(timer);
-  }, [timeLeft, gameFinished]);
+  }, [timeLeft, gameFinished, loading]);
   
   
 
@@ -130,7 +134,10 @@ export default function MovieQuiz() {
 
   return (
     <div>
-      {loading ? (<p>Cargando...</p>) :  (<div className="max-w-xl mx-auto p-10 bg-white shadow-lg rounded-lg text-center">
+      {loading || !currentQuestion ? (
+      <p>Cargando...</p>
+      ) : (
+      <div className="max-w-xl mx-auto p-10 bg-white shadow-lg rounded-lg text-center">
       <h2 className="text-2xl font-bold">{currentQuestion.question}</h2>
       <img src={currentQuestion.imageUrl} alt="Pregunta" className="w-full h-48 object-cover my-3 rounded" />
       <div className="grid grid-cols-1 gap-2">
