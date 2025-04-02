1680d29e9e3c162e1bfffeb00ed22e4cc6697425
UO222898 <UO222898@uniovi.es>
2025-04-02 17:56:27 +0200

    Modificada la barra de progreso

--- webapp/src/components/ProgressBar.js
+++ webapp/src/components/ProgressBar.js
@@ -1,19 +1,14 @@
-const ProgressBar = ({ timeLeft}) => {
-
+const ProgressBar = ({ timeLeft }) => {
   return (
     <div className="bg-orange rounded-lg overflow-hidden px-2 mx-4">
-      <div
-        className={`h-full transition-all duration-1000 ease-linear bg-blue-500`}
-        style={{ width: "100%" }}>
-        <progress
-          value={timeLeft}
-          max={60}
-          width = "100%"
-        />
-      </div>
+      <progress
+        className="progress-bar"
+        value={timeLeft}
+        max={60}
+        width="100%"
+      />
     </div>
   );
 };
-
 
 export default ProgressBar;
