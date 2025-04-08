a136f043499b0488c1511a944e602fa5cd7ad7b4
jmonteguillem <jmonteguillem@gmail.com>
2025-04-08 14:05:40 +0200

    Modificación del nav para la elección de idioma

--- webapp/src/components/NavMenu.jsx
+++ webapp/src/components/NavMenu.jsx
@@ -15,6 +15,23 @@ import {
   const NavMenu = ({ username }) => {
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
+
+    // Internacionalización:
+    const [langAnchorEl, setLangAnchorEl] = useState(null);
+    const langMenuOpen = Boolean(langAnchorEl);
+    const handleLangMenuOpen = (event) => {
+      setLangAnchorEl(event.currentTarget);
+    };
+  
+    const handleLangMenuClose = () => {
+      setLangAnchorEl(null);
+    };
+  
+    const changeLanguage = (lang) => {
+      console.log("Cambiar idioma a:", lang); // Más adelante se integra con i18n
+      setLangAnchorEl(null);
+    };
+  
   
     const handleMenuOpen = (event) => {
       setAnchorEl(event.currentTarget);
@@ -39,9 +56,52 @@ import {
             gridTemplateColumns: "repeat(5, 1fr)",
           }}
         >
-          <Button variant="primary" sx={{ color: "#fecd24", fontSize: "1.1rem" }}>
-            IDIOMA - ES
-          </Button>
+          <div>
+            <Button
+              onClick={handleLangMenuOpen}
+              sx={{ color: "#fecd24", fontSize: "1.1rem" }}
+            >
+              IDIOMA - ES
+            </Button>
+            <Menu
+              anchorEl={langAnchorEl}
+              open={langMenuOpen}
+              onClose={handleLangMenuClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              slotProps={{
+                paper: {
+                  sx: {
+                    backgroundColor: "#faf5ea",
+                    color: "#fecd24",
+                    boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.2)",
+                    width: "10em",
+                    border: "4px solid #c46331",
+                  },
+                },
+              }}
+            >
+              <MenuItem
+                onClick={() => changeLanguage("es")}
+                sx={{ backgroundColor: "#faf5ea", color: "#c46331", fontSize: "1.1rem" }}
+              >
+                Español
+              </MenuItem>
+              <MenuItem
+                onClick={() => changeLanguage("en")}
+                sx={{ backgroundColor: "#faf5ea", color: "#c46331", fontSize: "1.1rem" }}
+              >
+                Inglés
+              </MenuItem>
+            </Menu>
+          </div>
+
   
           <History username={username} />
   
