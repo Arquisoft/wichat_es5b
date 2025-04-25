6e622fa88ddd23ce17571d2feaef0bddf0f6bab6
AlbertoMO02 <alberto.molivar@gmail.com>
2025-04-25 21:43:47 +0200

    Arreglo tests que fallaban

--- webapp/src/components/PreguntasHistorico.test.js
+++ webapp/src/components/PreguntasHistorico.test.js
@@ -2,17 +2,20 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import PreguntasHistorico from './PreguntasHistorico';
+import { LanguageProvider } from '../LanguageContext';
 
 describe('PreguntasHistorico tests', () => {
+
+
   it('Se muestra el botón', () => {
     const user = { questions: [] };
-    render(<PreguntasHistorico user={user} />);
+    render(<LanguageProvider><PreguntasHistorico user={user} /></LanguageProvider>);
     expect(screen.getByText('Mostrar Preguntas')).toBeInTheDocument();
   });
   
   it('Se maneja el cierre del dialog', () => {
-    const user = { questions: [{ text: 'Pregunta 1', image: 'imagen1', option1: 'a', option2: 'b', option3: 'c', option4: 'd', correctOption: 'a'}] };
-    render(<PreguntasHistorico user={user} />);
+    const user = { questions: [{ question: {es:'Pregunta 1'}, image: 'imagen1', option1: 'a', option2: 'b', option3: 'c', option4: 'd', correctOption: 'a'}] };
+    render(<LanguageProvider><PreguntasHistorico user={user} /></LanguageProvider>);
     const button = screen.getByText('Mostrar Preguntas');
     fireEvent.click(button);
     expect(screen.getByText('Historial de Preguntas')).toBeInTheDocument();
