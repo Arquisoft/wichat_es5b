42feafe0fb7fa91f155093e6a3dd953ba7d87526
jmonteguillem <jmonteguillem@gmail.com>
2025-04-26 19:45:30 +0200

    Tests e2e "Crear Usuario" actualizados y funcionando

--- webapp/e2e/jest.config.js
+++ webapp/e2e/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    testMatch: ["**/steps/historic.steps.js"],
+    testMatch: ["**/steps/register-form.steps.js"],
     testTimeout: 120000,
     setupFilesAfterEnv: ["expect-puppeteer"]
 }
