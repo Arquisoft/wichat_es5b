6518723447e4d141e1e06122e89d3a527742861a
AlbertoMO02 <alberto.molivar@gmail.com>
2025-04-27 20:16:59 +0200

    se aumenta timeout y se asegura que la conexion exista en los tests

--- statsservice/gameRoutes.test.js
+++ statsservice/gameRoutes.test.js
@@ -10,6 +10,17 @@ const mongoose = require('mongoose');
 const server = require('./gameRoutes');
 const { Ranking, GameHistory } = require('./gameModels');
 
+jest.setTimeout(30000);
+
+beforeAll(async () => {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect('mongodb://localhost:27017/test', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+});
+
 
 afterAll(async () => {
   if (mongoose.connection.db) {
